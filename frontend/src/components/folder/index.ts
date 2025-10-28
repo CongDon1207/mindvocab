851fa09523b20c2a015a5e47d2926a754c674006
CongDon1207 <22133013@student.hcmute.edu.vn>
2025-10-28 16:47:44 +0700

    refactor component folder

---
+++ frontend/src/components/folder/index.ts
@@ -0,0 +1,10 @@
+// Re-export all folder-related components
+export { default as FolderList } from './FolderList'
+export { default as FolderCard } from './FolderCard'
+export { default as CreateFolderCard } from './CreateFolderCard'
+export { default as CreateFolderDialog } from './CreateFolderDialog'
+
+// Re-export types
+export type { Folder, FolderListProps } from './FolderList'
+export type { FolderCardProps } from './FolderCard'
+export type { CreateFolderValues } from './CreateFolderDialog'
